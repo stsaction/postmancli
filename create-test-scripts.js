eed0710b4fdc3d10814983a54b4033eac843b79c
stsaction <136581402+stsaction@users.noreply.github.com>
2023-07-05 13:34:22 +0530

    Update create-test-scripts.js

--- create-test-scripts.js
+++ create-test-scripts.js
@@ -1,4 +1,4 @@
-// test-script.js
+const { Assertion } = require('chai');
 
 pm.test("Status code is 201", function () {
   pm.response.to.have.status(201);
@@ -6,10 +6,10 @@ pm.test("Status code is 201", function () {
 
 pm.test("Response body has correct name", function () {
   var jsonData = pm.response.json();
-  pm.expect(jsonData.name).to.eql("morpheus");
+  Assertion.equal(jsonData.name, "morpheus");
 });
 
 pm.test("Response body has correct job", function () {
   var jsonData = pm.response.json();
-  pm.expect(jsonData.job).to.eql("leader");
+  Assertion.equal(jsonData.job, "leader");
 });
