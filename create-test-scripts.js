def4f20372b5b20840fccf8ec06672f498083445
stsaction <136581402+stsaction@users.noreply.github.com>
2023-07-05 13:07:21 +0530

    Create create-test-scripts.js

---
+++ create-test-scripts.js
@@ -0,0 +1,15 @@
+// test-script.js
+
+pm.test("Status code is 201", function () {
+  pm.response.to.have.status(201);
+});
+
+pm.test("Response body has correct name", function () {
+  var jsonData = pm.response.json();
+  pm.expect(jsonData.name).to.eql("morpheus");
+});
+
+pm.test("Response body has correct job", function () {
+  var jsonData = pm.response.json();
+  pm.expect(jsonData.job).to.eql("leader");
+});
