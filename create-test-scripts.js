6580fd9c2510d1e5a6eec71ac642aae8d7ca9e56
stsaction <136581402+stsaction@users.noreply.github.com>
2023-07-10 16:43:00 +0530

    Update create-test-scripts.js

--- create-test-scripts.js
+++ create-test-scripts.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 
 describe('API tests', function () {
   it('Status code is 201', function () {
-    const statusCode = 200;
+    const statusCode = 201;
     expect(statusCode).to.equal(201);
   });
 
