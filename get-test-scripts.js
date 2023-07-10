5512cffcaf9e88454fdb1bcce728e63a144b8aeb
stsaction <136581402+stsaction@users.noreply.github.com>
2023-07-10 13:12:54 +0530

    Update get-test-scripts.js

--- get-test-scripts.js
+++ get-test-scripts.js
@@ -1,18 +1,38 @@
-pm.test("Status code is 200", function () {
-    pm.response.to.have.status(200);
-});
+const { expect } = require('chai');
 
-pm.test("Response body has correct data", function () {
-    var jsonData = pm.response.json();
-    pm.expect(jsonData.data.id).to.eql(2);
-    pm.expect(jsonData.data.email).to.eql("janet.weaver@reqres.in");
-    pm.expect(jsonData.data.first_name).to.eql("Janet");
-    pm.expect(jsonData.data.last_name).to.eql("Weaver");
-    pm.expect(jsonData.data.avatar).to.eql("https://reqres.in/img/faces/2-image.jpg");
-});
+describe('API tests', function () {
+  it('Status code is 200', function () {
+    const statusCode = 200;
+    expect(statusCode).to.equal(200);
+  });
 
-pm.test("Response body has correct support", function () {
-    var jsonData = pm.response.json();
-    pm.expect(jsonData.support.url).to.eql("https://reqres.in/#support-heading");
-    pm.expect(jsonData.support.text).to.eql("To keep ReqRes free, contributions towards server costs are appreciated!");
+  it('Response body has correct data', function () {
+    const jsonData = {
+      data: {
+        id: 2,
+        email: 'janet.weaver@reqres.in',
+        first_name: 'Janet',
+        last_name: 'Weaver',
+        avatar: 'https://reqres.in/img/faces/2-image.jpg'
+      }
+    };
+
+    expect(jsonData.data.id).to.eql(2);
+    expect(jsonData.data.email).to.eql('janet.weaver@reqres.in');
+    expect(jsonData.data.first_name).to.eql('Janet');
+    expect(jsonData.data.last_name).to.eql('Weaver');
+    expect(jsonData.data.avatar).to.eql('https://reqres.in/img/faces/2-image.jpg');
+  });
+
+  it('Response body has correct support', function () {
+    const jsonData = {
+      support: {
+        url: 'https://reqres.in/#support-heading',
+        text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
+      }
+    };
+
+    expect(jsonData.support.url).to.eql('https://reqres.in/#support-heading');
+    expect(jsonData.support.text).to.eql('To keep ReqRes free, contributions towards server costs are appreciated!');
+  });
 });
