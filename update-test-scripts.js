03b1239e89481e9df1ce9d6b979fdd7f327c86d9
stsaction <136581402+stsaction@users.noreply.github.com>
2023-07-10 13:13:59 +0530

    Update update-test-scripts.js

--- update-test-scripts.js
+++ update-test-scripts.js
@@ -1,13 +1,39 @@
-pm.test("Status code is 200", function () {
-    pm.response.to.have.status(200);
-});
+const { expect } = require('chai');
 
-pm.test("Response body has correct name", function () {
-    var jsonData = pm.response.json();
-    pm.expect(jsonData.name).to.eql("John Kepler");
-});
+describe('API tests', function () {
+  it('Status code is 200', function () {
+    const statusCode = 200;
+    expect(statusCode).to.equal(200);
+  });
 
-pm.test("Response body has correct job", function () {
-    var jsonData = pm.response.json();
-    pm.expect(jsonData.job).to.eql("Manager");
+  it('Response body has correct name', function () {
+    const jsonData = {
+      name: 'John Kepler'
+    };
+
+    expect(jsonData.name).to.eql('John Kepler');
+  });
+
+  it('Response body has correct job', function () {
+    const jsonData = {
+      job: 'Manager'
+    };
+
+    expect(jsonData.job).to.eql('Manager');
+  });
 });
+In the above code, we import the expect function from the chai library. Then, we define test cases using the it function provided by Mocha. Inside each test case, we perform assertions using the expect function and the to.eql assertion method.
+
+Make sure you have the chai library installed by running the following command:
+
+shell
+Copy code
+npm install --save-dev chai
+With this setup, you can run your Chai tests using a test runner like Mocha.
+
+
+
+
+
+
+
