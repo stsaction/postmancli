8c9fdea3b00134c2afc14cc6fbeab515a5a70489
stsaction <136581402+stsaction@users.noreply.github.com>
2023-07-10 11:23:52 +0530

    Update create-test-scripts.js

--- create-test-scripts.js
+++ create-test-scripts.js
@@ -1,18 +1,15 @@
 const { Assertion } = require('chai');
 
 pm.test("Status code is 200", function () {
-  // Change the expected status code to 500 to intentionally fail
-  pm.response.to.have.status(500);
+  pm.response.to.have.status(200);
 });
 
-pm.test("Response body has correct name", function () {
+   pm.test("Response body has correct name", function () {
   var jsonData = pm.response.json();
-  // Change the expected name to "wrong_name" to intentionally fail
-  Assertion.equal(jsonData.name, "wrong_name");
+  Assertion.equal(jsonData.name, "morpheus");
 });
 
 pm.test("Response body has correct job", function () {
   var jsonData = pm.response.json();
-  // Change the expected job to "wrong_job" to intentionally fail
-  Assertion.equal(jsonData.job, "wrong_job");
+  Assertion.equal(jsonData.job, "leader");
 });
